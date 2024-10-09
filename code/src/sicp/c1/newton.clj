439cdc9fe256c62a1418084fff8aa0914d34227c
Pablo Fernandez <fernandezpablo85@gmail.com>
2024-10-09 12:53:22 -0300

    up to 1.8

---
+++ code/src/sicp/c1/newton.clj
@@ -0,0 +1,40 @@
+(ns sicp.c1.newton)
+
+(def err 0.000001)
+
+(defn square [x] (* x x))
+(defn cube [x] (* x x x))
+
+(defn avg [x y]
+  (/ (+ x y) 2.0))
+
+;; square root
+(defn improve [guess x]
+  (avg guess (/ x guess)))
+
+(defn good-enough? [prev-guess current-guess]
+  (< (Math/abs (- current-guess prev-guess)) err))
+
+(defn sqrt-iter [guess x]
+  (if (good-enough? guess (improve guess x))
+    guess
+    (recur (improve guess x) x)))
+
+(defn sqrt [x]
+  (sqrt-iter 1.0 x))
+
+(square (sqrt 16))
+
+;; cubic root
+(defn improve-cube [guess x]
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+
+(defn cube-iter [guess x]
+  (if (good-enough? guess (improve-cube guess x))
+    guess
+    (recur (improve-cube guess x) x)))
+
+(defn cube-root [x]
+  (cube-iter 1.0 x))
+
+(cube (cube-root 9))
