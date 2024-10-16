ba48c7f72dda611d26027db0257f701a18115488
Pablo Fernandez <fernandezpablo85@gmail.com>
2024-10-16 20:53:49 -0300

    all primes exercises

--- code/src/sicp/c1/primes.clj
+++ code/src/sicp/c1/primes.clj
@@ -6,11 +6,12 @@
 (defn divisible? [x y] (== (mod x y) 0))
 
 (defn smallest-divisor [x]
-  (letfn [(iter [current]
+  (letfn [(next [n] (if (== 2 n) 3 (+ 2 n)))
+          (iter [current]
             (cond
               (> (square current) x) x
               (divisible? x current) current
-              :else (recur (inc current))))]
+              :else (recur (next current))))]
     (iter 2)))
 
 (defn prime? [x] (== x (smallest-divisor x)))
@@ -63,8 +64,91 @@
       796
       9
       1892
+      ;; 561 Carmichael Number 😲
       10)))
 
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;; Exercise 1.21 ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(assert (== (smallest-divisor 199) 199))
+(assert (== (smallest-divisor 1999) 1999))
+(assert (== (smallest-divisor 19999) 7))
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;; Exercise 1.22 ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn runtime [] (long (/ (System/nanoTime) 1000)))
+
+
+(defn report-prime [n elapsed]
+  (println " *** ")
+  (println n)
+  (println elapsed)
+  :true)
+
+(defn start-prime-test [n start]
+  (if (fast-prime? n)
+    (report-prime n (- (runtime) start))))
+
+(defn timed-prime-test [n]
+  (start-prime-test n (runtime)))
+
+(defn search-for-primes [start n]
+  (if (== 0 n)
+    (println "done")
+    (if (timed-prime-test start)
+      (search-for-primes (inc start) (dec n))
+      (search-for-primes (inc start) n))))
+
+;; (search-for-primes 10000 10)
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;; Exercise 1.27 ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn is-congruent? [n]
+  (map #(== (exp-mod % n n) %) (range 1 n)))
+
+(every? identity (is-congruent? 561))
+(every? identity (is-congruent? 1105))
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;; Exercise 1.28 ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn exp-mod-root
+  "computes x^y mod m and checks for non-trivial square roots"
+  [x y m]
+
+  (cond
+    (== 0 y) 1
+    (odd? y) (mod (*  x (exp-mod-root x (dec y) m)) m)
+    :else
+    (if (and (== (mod (square x) m) 1) (not= (mod x m) 1) (not= (mod x m) (- m 1)))
+      0
+      (mod (square (exp-mod-root x (/ y 2) m)) m))))
+
+(defn miller-rabin?
+  ([n] (miller-rabin? n 10))
+  ([n times]
+   (letfn [(rand-a [] (+ (rand-int (- n 1)) 1))
+           (looks-prime? [] (== (exp-mod-root (rand-a) (- n 1) n) 1))]
+     (every? identity (map (fn [_] (looks-prime?)) (range times))))))
+
+(deftest carmichael-numbers-test
+  (testing "checks that miller rabin catches charmicael composites"
+    (are [n] (false? (miller-rabin? n 50))
+      561
+      1105
+      1729
+      2465
+      2821
+      6601
+      8911
+      10585
+      15841
+      29341)))
+
 (run-tests 'sicp.c1.primes)
-
-
