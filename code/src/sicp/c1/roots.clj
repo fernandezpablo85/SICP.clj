1db3f4d215dd57cce0e45431f0b3abc0326c12d9
Pablo Fernandez <fernandezpablo85@gmail.com>
2024-10-19 00:20:53 -0300

    bump clojure version, update up to ex 1.37

--- code/src/sicp/c1/roots.clj
+++ code/src/sicp/c1/roots.clj
@@ -1,6 +1,5 @@
-(ns sicp.c1.roots)
-
-(defn abs [n] (max n (- n)))
+(ns sicp.c1.roots
+  (:require [clojure.math :refer [log]]))
 
 (defn is-close? [a b]
   (< (abs (- a b)) 0.001))
@@ -46,7 +45,55 @@
 (defn broken-sqrt [x]
   (fixed-point #(/ x %) 1.0))
 
-;; (broken-sqrt 16) – does not converge
+;; (broken-sqrt 16) – does not converge, oscillates between y1 and y3
+
+;; average (x / y) with y => (y + (x / y)) / 2
+(defn fixed-sqrt [x]
+  (fixed-point #(/ (+ % (/ x %)) 2) 1.0))
+
+(fixed-sqrt 16)
+
+;; golden ratio apprx
+(defn golden-ratio []
+  (fixed-point #(+ 1 (/ 1 %)) 7))
+
+(float (golden-ratio))
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;; Exercise 1.36 ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn x-square-est [x]
+  (println x)
+  (/ (log 1000) (log x)))
+
+(defn x-square-est-avg [x]
+  (println x)
+  (/ (+ x (/ (log 1000) (log x))) 2))
 
 
+(fixed-point x-square-est-avg 5)
 
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;; Exercise 1.37 ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn cont-frac
+  ([n d k] (cont-frac n d k 0))
+  ([n d k i]
+   (if (= i k)
+     (/ (n i) (d i))
+     (/ (n i) (+ (d i) (cont-frac n d k (inc i)))))))
+
+(defn cont-frac-iter
+  ([n d k] (cont-frac n d k 0))
+  ([n d k i]
+   (loop [i i
+          total (/ (n i) (d i))]
+     (if (= i k)
+       total
+       (recur (inc i) (/ (n i) (+ (d i) total)))))))
+
+(defn one [_] 1.0)
+(/ 1 (cont-frac one one 11))
+(/ 1 (cont-frac-iter one one 11))
